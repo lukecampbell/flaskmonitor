5f3ae073762cb3bb79cb071e0e611b4a4101173b
Luke Campbell <luke.s.campbell@gmail.com>
2013-01-11 14:32:47 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import flaskmonitor
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+packages = ['flaskmonitor']
+requires = [
+        'flask==0.9',
+        'python-cjson==1.0.5',
+        'numpy'
+        ]
+
+
+setup(
+        name='flaskmonitor',
+        version=flaskmonitor.__version__,
+        description='Flask application to monitor running processes',
+        long_description=open('README.md').read(),
+        author='Luke Campbell',
+        author_email='lcampbell@asascience.com',
+        url='https://github.com/lukecampbell/flaskmonitor.git',
+        packages=packages,
+        package_data={'':['LICENSE']},
+        include_package_data=True,
+        install_requires=requires,
+        license=open("LICENSE").read(),
+        classifiers=(
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            ),
+        )
+
+
+
